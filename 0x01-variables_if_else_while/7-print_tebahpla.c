3253fddb8ec99ac40482809873ce6c825681683d
hollymcsaint <hollymcsaint@gmail.com>
2022-09-09 16:41:02 -0700

    Varibales

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,39 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *   * main - Prints the alphabet at reverse
+ *
+ *     *
+ *
+ *       * Return: Always (Success)
+ *
+ *         */
+
+int main(void)
+
+{
+
+		char c;
+
+
+
+			for (c = 'z'; c >= 'a'; c--)
+
+					{
+
+								putchar(c);
+
+									}
+
+
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
