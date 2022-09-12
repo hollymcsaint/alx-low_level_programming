487c56025c5a076f6bd76c4559b8ac8c47b59bae
hollymcsaint <hollymcsaint@gmail.com>
2022-09-12 13:45:00 -0700

    Print combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,28 +17,28 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 
-	{
+		{
 		for (i = '0'; i <= '9'; i++)
 
-		{
-
-	if (c < i)
-
 			{
+
+			if (c < i)
+
+				{
 				putchar(c);
 				putchar(i);
 
-	if (c != '8' || (c == '8' && i != '9'))
+					if (c != '8' || (c == '8' && i != '9'))
 
-				{
-					putchar(',');
-					putchar(' ');
+					{	
+						putchar(',');
+						putchar(' ');
+					}	
 				}
-			}
+
+			}	
 
 		}
-
-	}
 
 				putchar('\n');
 
