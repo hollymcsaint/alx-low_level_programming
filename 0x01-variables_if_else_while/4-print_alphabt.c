10baf1ae6e7fe19be14775de94da758b86a4fbde
hollymcsaint <hollymcsaint@gmail.com>
2022-09-12 06:41:36 -0700

    Print Alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,39 +3,28 @@
 
 
 /**
+ *main - Prints some letters of alphabet
  *
- *   * main - Prints some letters of alphabet
+ *Return: Always (Success)
  *
- *     *
- *
- *       * Return: Always (Success)
- *
- *         */
+**/
 
 int main(void)
 
 {
 
-		char c;
+	char c;
 
+		for (c = 'a'; c <= 'z'; c++)
 
+	{
+		if (c != 'e' && c != 'q')
 
-			for (c = 'a'; c <= 'z'; c++)
+		{
 
-					{
-
-								if (c != 'e' && c != 'q')
-
-											{
-
-															putchar(c);
-
-																	}
-
-									}
-
-
-
+												putchar(c);
+												}
+							}
 				putchar('\n');
 
 
