f1e785ba7cee051d81f44900bf708567b9cc2e34
hollymcsaint <hollymcsaint@gmail.com>
2022-09-12 07:07:17 -0700

    Print Alpha 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
 
-
-
 /**
+ *main - Prints the numbers since 0 to 9
  *
- *   * main - Prints the numbers since 0 to 9
  *
- *     *
+ * Return: Always (Success)
  *
- *       * Return: Always (Success)
- *
- *         */
+ */
 
 int main(void)
 
 {
-
-		char n;
-
-
-
-			for (n = '0'; n <= '9'; n++)
-
-					{
-
-								putchar(n);
-
-									}
+	char n;
+	for  (n = '0'; n =< '9'; n++)
+	
+	{
+		putchar(n);
 
 
+	}
 
-				putchar('\n');
+		putchar('\n');
 
-
-
-					return (0);
+		return (0);
 
 }
 
