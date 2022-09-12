a62c49308cb24f4c2f821cf1315ea86cd6c5c928
hollymcsaint <hollymcsaint@gmail.com>
2022-09-12 06:52:34 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,20 +15,18 @@ int main(void)
 
 	char c;
 
-		for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 
 	{
-	
+
 	if (c != 'e' && c != 'q')
 
 		{
-
 												putchar(c);
 												}
 	}
 
 		putchar('\n');
-
 		return (0);
 
 }
