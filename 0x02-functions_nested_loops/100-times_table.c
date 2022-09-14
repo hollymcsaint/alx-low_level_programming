8d38f7b54df49e090b11c39e0849c9864ce07901
hollymcsaint <hollymcsaint@gmail.com>
2022-09-14 10:18:21 -0700

    print ntables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,6 @@
 void print_times_table(int n)
 
 {
-
 	int x, y, z;
 
 	if (n >= 0 && n <= 14)
@@ -30,36 +29,28 @@ void print_times_table(int n)
 					_putchar((z / 100) + '0');
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-
 				}
 				else if (z > 9)
 				{
-
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
 					_putchar(((z / 10) % 10) + '0');
 					_putchar((z % 10) + '0');
-
 				}
 				else
 				{
-
 					if (y != 0)
 					{
-
 						_putchar(',');
 						_putchar(32);
 						_putchar(32);
 						_putchar(32);
-
 					}
 					_putchar(z + '0');
 				}
-
 			}
 			_putchar('\n');
 		}
 	}
-
 }
