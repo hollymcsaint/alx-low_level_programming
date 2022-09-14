dbd852c9d72ebef246e45b0e72ed27e62f2b41f9
hollymcsaint <hollymcsaint@gmail.com>
2022-09-14 08:36:01 -0700

    function for +,- and 0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *
+ *  * print_sign - a function that prints the sign of a number
+ *
+ *   * @n: number input
+ *
+ *Return: 1 prints '+', 0 prints '0', -1 prints '-'.
+ *
+ */
+
+int print_sign(int n)
+
+{
+
+	if (n > 0)
+
+	{
+		_putchar ('+');
+
+		return (1);
+	}
+
+	else if (n == 0)
+
+	{
+
+		_putchar ('0');
+
+		return (0);
+
+	}
+
+	else
+
+	{
+
+		_putchar ('-');
+
+		return (-1);
+
+	}
+
+}
