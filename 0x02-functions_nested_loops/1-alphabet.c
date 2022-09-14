1313a728f67a1ba57caf6445d765b99438422587
hollymcsaint <hollymcsaint@gmail.com>
2022-09-14 04:36:52 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,25 +4,18 @@
  *  * print_alphabet - Make the alphabet
  *
  *    * Return: void
- *
  */
 
 void print_alphabet(void)
 
 {
+	char c;
 
-		char c;
+	for (c = 'a'; c <= 'z'; c++)
 
-		for (c = 'a'; c <= 'z'; c++)
-
-			{
-
-				_putchar(c);
-
-			}
-
-
+		{
+			_putchar(c);
+		}
 
 		_putchar('\n');
-
 }
