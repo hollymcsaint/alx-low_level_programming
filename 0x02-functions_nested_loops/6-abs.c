1c3b20dbec3f7bc7ac933ff9b07410baa2d57b57
hollymcsaint <hollymcsaint@gmail.com>
2022-09-14 08:51:11 -0700

    function that compute integers

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,13 +3,13 @@
 /**
  *  * _abs - a function that computes the absolute value of an integer.
  *
- *   * @n: integer input
+ *   * @ab: integer input
  *
- *    * Return: absolute value of an integer.
+ *    * Return: absolute value of an integer ab.
  *
  */
 
-int _abs(int);
+int _abs(int ab)
 
 {
 	return (ab * ((ab > 0) - (ab < 0)));
