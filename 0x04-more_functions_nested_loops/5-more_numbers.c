3e168c897809aa25b5176c8613a812abecc948d9
hollymcsaint <hollymcsaint@gmail.com>
2022-09-16 01:44:50 -0700

    More function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *  * more_numbers - prints numbers between 0 to 14
+ *
+ *   * 10 times.
+ *
+ *    * Return: no return.
+ *
+ */
+
+void more_numbers(void)
+
+{
+	int i, a;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 0; a < 15; a++)
+		{
+			if (a >= 10)
+				_putchar((a / 10) + 48);
+			_putchar((a % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
