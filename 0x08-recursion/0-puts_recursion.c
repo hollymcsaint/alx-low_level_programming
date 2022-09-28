ee1a4a811cd944ca226110e798c6d2eac5e37cf5
hollymcsaint <hollymcsaint@gmail.com>
2022-09-28 06:34:05 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - A function that print a string follow by a new name.
+ *
+ *@s - String to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion(s++);
+}
+
+
+
+
+
+
+
+
