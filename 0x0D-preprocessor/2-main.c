7b4c78e21f4434144a2e070ac8841d60bfa1c0b2
hollymcsaint <hollymcsaint@gmail.com>
2022-09-30 09:25:31 +0100

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,7 +4,7 @@
  * main - Program that prints the name of the file it was compiled from followed 
  * by a new line
  *
- * Return: zero (Success)
+ * Return: always 0
  *
  */
 
@@ -12,5 +12,5 @@ int main(void)
 {
 	printf("%s\n", __FILE__);
 	
-	return 0;
+	return (0);
 }
