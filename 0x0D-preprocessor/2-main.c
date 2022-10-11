aaf091269e2b0094b7be0bf5a5e91dfce66f1978
hollymcsaint <hollymcsaint@gmail.com>
2022-10-11 07:00:20 -0700

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Program that prints the name of the file it was compiled from followed
- * by a new line
+ * main - Program that prints the name of the file it was compiled from
  *
  * Return: always 0
  *
